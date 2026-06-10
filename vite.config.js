7abaf815328bda16e49f01e690955ed669985cd2
TuNombre <tuemail@loquesea.com>
2026-06-10 14:07:07 +0200

    fix vite config for production

--- vite.config.js
+++ vite.config.js
@@ -3,16 +3,7 @@ import react from "@vitejs/plugin-react";
 
 export default defineConfig({
   plugins: [react()],
-  server: {
-    host: "0.0.0.0",
-    port: 5173,
-    strictPort: true,
-    hmr: {
-      protocol: "ws",
-      host: "192.168.18.100", // tu IP local
-      port: 5173,
-    },
-  },
+  base: "./",
   build: {
     outDir: "dist",
   },
