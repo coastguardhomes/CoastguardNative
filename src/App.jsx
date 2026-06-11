66bca246db5cf81d0363f0e6a9024e7c83bcd5ec
coastguardhomes <TU_EMAIL>
2026-06-11 18:09:52 +0200

    Fix: eliminar window.onerror que generaba '0' en pantalla

--- src/App.jsx
+++ src/App.jsx
@@ -2,18 +2,6 @@ import { useEffect } from "react";
 import AppRouter from "./router/AppRouter";
 import "./styles/global.css";
 
-// 🔥 MOSTRAR ERRORES EN PANTALLA (para detectar la pantalla negra)
-window.onerror = (msg, url, line, col, error) => {
-  alert(
-    "JS ERROR:\n" +
-      msg +
-      "\nArchivo: " +
-      url +
-      "\nLínea: " +
-      line
-  );
-};
-
 export default function App() {
   useEffect(() => {
     console.log("CoastGuardApp iniciada");
