bd188cf1f51467417f6c2dece038873d6478266e
coastguardhomes <TU_EMAIL>
2026-06-13 23:05:51 +0200

    remove server from vite config

--- vite.config.js
+++ vite.config.js
@@ -4,10 +4,6 @@ import react from "@vitejs/plugin-react";
 export default defineConfig({
   base: "./",
   plugins: [react()],
-  server: {
-    host: true,
-    port: 5173,
-  },
   build: {
     outDir: "dist",
     emptyOutDir: true,
