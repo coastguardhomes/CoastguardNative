18285087374d44d721c2e5ad30fa7b7a6f8c8ed7
coastguardhomes <TU_EMAIL>
2026-06-13 20:36:48 +0200

    fix splash main

--- src/main.jsx
+++ src/main.jsx
@@ -1,8 +1,13 @@
 import React from "react";
 import ReactDOM from "react-dom/client";
 import App from "./App.jsx";
-import "./styles/global.css";
+
+// Ocultar splash screen (Capacitor 5)
+import { SplashScreen } from "@capacitor/splash-screen";
+SplashScreen.hide();
 
 ReactDOM.createRoot(document.getElementById("root")).render(
-  <App />
+  <React.StrictMode>
+    <App />
+  </React.StrictMode>
 );
