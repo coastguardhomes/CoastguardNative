52d5ab764c889ad442ab0c95e4495a78c12ffb55
coastguardhomes <coastguardhomes@gmail.com>
2026-07-18 22:14:36 +0200

    Add monthly maintenance prices to constants

--- src/constants/prices.js
+++ src/constants/prices.js
@@ -1,9 +1,16 @@
 export const PRICES = {
+  // Servicios individuales
   emergencia: 50,
   apertura: 30,
   cierre: 30,
   supervision: 35,
   gestionTecnico: 25,
   visitaRapida: 25,
-  postTormenta: 35
+  postTormenta: 35,
+
+  // Mantenimiento mensual
+  mantenimientoBasico: 29,
+  mantenimientoEstandar: 49,
+  mantenimientoPremium: 69,
+  custodiaLlaves: 15
 };
