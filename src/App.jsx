08231683bb09b34d133bddf48d8d1f4b7e48f25e
coastguardhomes <coastguardhomes@gmail.com>
2026-07-18 19:27:24 +0200

    Refactor routing structure and add RequireAuth

--- src/App.jsx
+++ src/App.jsx
@@ -1,45 +1,186 @@
-import { Navigate, Route, Routes } from "react-router-dom";
+import React from "react";
+import { BrowserRouter, Routes, Route } from "react-router-dom";
 
+import Login from "./pages/login/Login";
 import Inicio from "./pages/inicio/Inicio";
-import Clientes from "./pages/clientes/Clientes";
-import Tecnicos from "./pages/tecnicos/Tecnicos";
-import Viviendas from "./pages/viviendas/Viviendas";
-import Inspecciones from "./pages/inspecciones/Inspecciones";
-import Contratos from "./pages/contratos/Contratos";
-import FacturasLista from "./pages/facturas/FacturasLista";
-import FiltrosFacturas from "./pages/facturas/FiltrosFacturas";
-import EstadisticasFacturas from "./pages/facturas/EstadisticasFacturas";
-import VerFactura from "./pages/facturas/VerFactura";
-import Ajustes from "./pages/Ajustes/Ajustes";
-import Login from "./pages/Login/Login";
-import ResetPassword from "./pages/auth/ResetPassword";
-import UpdatePassword from "./pages/auth/UpdatePassword";
 
-import Extras from "./pages/extras/Extras";
+import Contratos from "./pages/contratos/Contratos";
+import CrearContrato from "./pages/contratos/CrearContrato";
+import EditarContrato from "./pages/contratos/EditarContrato";
+import VerContrato from "./pages/contratos/VerContrato";
+
+import Clientes from "./pages/clientes/Clientes";
+import CrearCliente from "./pages/clientes/CrearCliente";
+import EditarCliente from "./pages/clientes/EditarCliente";
+import VerCliente from "./pages/clientes/VerCliente";
+
+import Viviendas from "./pages/viviendas/Viviendas";
+import CrearVivienda from "./pages/viviendas/CrearVivienda";
+import EditarVivienda from "./pages/viviendas/EditarVivienda";
+import VerVivienda from "./pages/viviendas/VerVivienda";
+
+import Tecnicos from "./pages/tecnicos/Tecnicos";
+import CrearTecnico from "./pages/tecnicos/CrearTecnico";
+import EditarTecnico from "./pages/tecnicos/EditarTecnico";
+import VerTecnico from "./pages/tecnicos/VerTecnico";
+
+import RequireAuth from "./components/auth/RequireAuth";
 
 export default function App() {
   return (
-    <Routes>
-      <Route path="/" element={<Inicio />} />
-      <Route path="/login" element={<Login />} />
-      <Route path="/reset-password" element={<ResetPassword />} />
-      <Route path="/update-password" element={<UpdatePassword />} />
+    <BrowserRouter>
+      <Routes>
 
-      <Route path="/clientes" element={<Clientes />} />
-      <Route path="/tecnicos" element={<Tecnicos />} />
-      <Route path="/viviendas" element={<Viviendas />} />
-      <Route path="/contratos" element={<Contratos />} />
-      <Route path="/inspecciones" element={<Inspecciones />} />
+        {/* LOGIN ES LA PANTALLA INICIAL */}
+        <Route path="/" element={<Login />} />
 
-      <Route path="/facturas" element={<FacturasLista />} />
-      <Route path="/facturas/filtros" element={<FiltrosFacturas />} />
-      <Route path="/facturas/estadisticas" element={<EstadisticasFacturas />} />
-      <Route path="/facturas/ver/:id" element={<VerFactura />} />
+        {/* RUTAS PRIVADAS */}
+        <Route
+          path="/inicio"
+          element={
+            <RequireAuth>
+              <Inicio />
+            </RequireAuth>
+          }
+        />
 
-      <Route path="/extras" element={<Extras />} />
-      <Route path="/ajustes" element={<Ajustes />} />
+        {/* CONTRATOS */}
+        <Route
+          path="/contratos"
+          element={
+            <RequireAuth>
+              <Contratos />
+            </RequireAuth>
+          }
+        />
+        <Route
+          path="/contratos/crear"
+          element={
+            <RequireAuth>
+              <CrearContrato />
+            </RequireAuth>
+          }
+        />
+        <Route
+          path="/contratos/editar/:id"
+          element={
+            <RequireAuth>
+              <EditarContrato />
+            </RequireAuth>
+          }
+        />
+        <Route
+          path="/contratos/ver/:id"
+          element={
+            <RequireAuth>
+              <VerContrato />
+            </RequireAuth>
+          }
+        />
 
-      <Route path="*" element={<Navigate to="/" replace />} />
-    </Routes>
+        {/* CLIENTES */}
+        <Route
+          path="/clientes"
+          element={
+            <RequireAuth>
+              <Clientes />
+            </RequireAuth>
+          }
+        />
+        <Route
+          path="/clientes/crear"
+          element={
+            <RequireAuth>
+              <CrearCliente />
+            </RequireAuth>
+          }
+        />
+        <Route
+          path="/clientes/editar/:id"
+          element={
+            <RequireAuth>
+              <EditarCliente />
+            </RequireAuth>
+          }
+        />
+        <Route
+          path="/clientes/ver/:id"
+          element={
+            <RequireAuth>
+              <VerCliente />
+            </RequireAuth>
+          }
+        />
+
+        {/* VIVIENDAS */}
+        <Route
+          path="/viviendas"
+          element={
+            <RequireAuth>
+              <Viviendas />
+            </RequireAuth>
+          }
+        />
+        <Route
+          path="/viviendas/crear"
+          element={
+            <RequireAuth>
+              <CrearVivienda />
+            </RequireAuth>
+          }
+        />
+        <Route
+          path="/viviendas/editar/:id"
+          element={
+            <RequireAuth>
+              <EditarVivienda />
+            </RequireAuth>
+          }
+        />
+        <Route
+          path="/viviendas/ver/:id"
+          element={
+            <RequireAuth>
+              <VerVivienda />
+            </RequireAuth>
+          }
+        />
+
+        {/* TECNICOS */}
+        <Route
+          path="/tecnicos"
+          element={
+            <RequireAuth>
+              <Tecnicos />
+            </RequireAuth>
+          }
+        />
+        <Route
+          path="/tecnicos/crear"
+          element={
+            <RequireAuth>
+              <CrearTecnico />
+            </RequireAuth>
+          }
+        />
+        <Route
+          path="/tecnicos/editar/:id"
+          element={
+            <RequireAuth>
+              <EditarTecnico />
+            </RequireAuth>
+          }
+        />
+        <Route
+          path="/tecnicos/ver/:id"
+          element={
+            <RequireAuth>
+              <VerTecnico />
+            </RequireAuth>
+          }
+        />
+
+      </Routes>
+    </BrowserRouter>
   );
 }
