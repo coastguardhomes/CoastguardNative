286a8c335b7fddc8a79ea95c6468f267a2f4604a
coastguardhomes <coastguardhomes@gmail.com>
2026-07-18 21:16:35 +0200

    Refactor import statement for Login component

--- src/App.jsx
+++ src/App.jsx
@@ -2,7 +2,7 @@ import React, { useEffect, useState } from "react";
 import { BrowserRouter as Router, Routes, Route, Navigate } from "react-router-dom";
 import { supabase } from "./supabaseClient";
 
-// AUTH (solo existe Login.jsx)
+// AUTH
 import Login from "./pages/Login/Login.jsx";
 
 // CLIENTES
