d3974ed661856f47c4b24fc30f5b1fc8b7477382
coastguardhomes <coastguardhomes@gmail.com>
2026-08-09 03:05:54 +0200

    Register CameraPlugin in MainActivity
    Added CameraPlugin registration in MainActivity.

--- android/app/src/main/java/com/coastguard/mobile/MainActivity.java
+++ android/app/src/main/java/com/coastguard/mobile/MainActivity.java
@@ -1,5 +1,19 @@
 package com.coastguard.mobile;
 
+import android.os.Bundle;
 import com.getcapacitor.BridgeActivity;
+import com.getcapacitor.Plugin;
 
-public class MainActivity extends BridgeActivity {}
+// ⭐ IMPORTANTE: importar el plugin de cámara
+import com.capacitorjs.plugins.camera.CameraPlugin;
+
+public class MainActivity extends BridgeActivity {
+
+    @Override
+    public void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+
+        // ⭐ Registrar plugins nativos
+        registerPlugin(CameraPlugin.class);
+    }
+}
