71d7c6d24fe8b271a02c5af20432dcc25abb72f9
coastguardhomes <coastguardhomes@gmail.com>
2026-09-12 13:59:25 +0200

    Update email redirect URL in signUp options

--- src/pages/Register/Register.jsx
+++ src/pages/Register/Register.jsx
@@ -59,7 +59,7 @@ export default function Register() {
       email,
       password,
       options: {
-        emailRedirectTo: "https://coastguardhomes.es/auth/callback",
+        emailRedirectTo: "https://coastguardnative.vercel.app/auth/callback",
       },
     });
 
